8a07c406e64d7a78683528d2bb91091079368575
radioactive4u <lukecrawfordkaslo@gmail.com>
2025-07-13 16:58:42 -0700

    Cleaned and rebuilt project for correct Vercel deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import RoutePlannerApp from "./RoutePlannerApp";
-import "leaflet/dist/leaflet.css";
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RoutePlannerApp from './RoutePlannerApp';
+import 'leaflet/dist/leaflet.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
   <React.StrictMode>
     <RoutePlannerApp />
   </React.StrictMode>
