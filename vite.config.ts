936c2836969d792fcb433e1fb35a97576218b9bd
radioactive4u <lukecrawfordkaslo@gmail.com>
2025-07-13 13:30:51 -0700

    Fix Vite config for working Vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- THIS IS THE CRITICAL FIX
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     outDir: 'dist',
   },
-})
+});
